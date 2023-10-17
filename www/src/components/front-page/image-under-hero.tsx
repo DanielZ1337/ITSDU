56cbc635c7c4925c134f586a372f94075e6f7971
DanielZ1337 <danielz2nd@hotmail.com>
2023-10-17 12:36:22 +0200

    Update image-under-hero.tsx

--- www/src/components/front-page/image-under-hero.tsx
+++ www/src/components/front-page/image-under-hero.tsx
@@ -4,7 +4,7 @@ export default function ImageUnderHero() {
 
     return (
         <div
-            className="hover:scale-[100.5%] animate-in duration-1000 max-w-full mx-auto my-32 overflow-hidden rounded-lg border bg-foreground/10 p-4 hover:shadow-2xl hover:shadow-primary/10 shadow-lg shadow-primary/5 backdrop-blur-3xl slide-in-from-bottom-3/4"
+            className="hover:scale-[100.5%] animate-in duration-1000 max-w-full mx-auto my-32 overflow-hidden rounded-lg border bg-foreground/10 p-4 hover:shadow-2xl hover:dark:shadow-primary/10 hover:shadow-primary/20 shadow-lg dark:shadow-primary/5 shadow-primary/15 backdrop-blur-3xl slide-in-from-bottom-3/4"
         >
             <Image src={image_home} alt="Course Home" className="rounded-lg border animate-in fade-in-0 duration-1000 flex justify-left w-full h-full object-cover" />
         </div>
