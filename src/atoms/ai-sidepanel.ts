ce2416572a44ff0d77dd7e24e5102b228afdc9ab
DanielZ1337 <danielz2nd@hotmail.com>
2023-12-09 23:13:58 +0100

    fix settings

--- src/atoms/ai-sidepanel.ts
+++ src/atoms/ai-sidepanel.ts
@@ -1,4 +1,6 @@
-import { SettingsOptions } from "@/types/settings";
+import { SettingsOptions, defaultSettings } from "@/types/settings";
 import { atom } from "jotai";
 
-export const aiSidepanelAtom = atom((JSON.parse(localStorage.getItem('settings') as string) as SettingsOptions).DefaultAIChatSidepanel)
+export const aiSidepanelAtom = atom(localStorage.getItem('settings')
+    ? (JSON.parse(localStorage.getItem('settings')!) as SettingsOptions).DefaultAIChatSidepanel
+    : defaultSettings.DefaultAIChatSidepanel)
--- src/hooks/atoms/useSettings.ts
+++ src/hooks/atoms/useSettings.ts
@@ -1,9 +1,24 @@
 import { asyncSettingsAtom, settingsAtom } from '@/atoms/settings';
-import { SettingsOptions } from '@/types/settings';
+import { SettingsOptions, defaultSettings } from '@/types/settings';
 import { useAtom } from 'jotai';
 
 export function useSettings() {
     const [settings, setSettings] = useAtom(settingsAtom);
+
+    if (!localStorage.getItem('settings')) {
+        localStorage.setItem('settings', JSON.stringify(defaultSettings));
+    }
+
+    // check all keys in settings and add any missing ones
+    const localStorageSettings = JSON.parse(localStorage.getItem('settings')!);
+    const localStorageSettingsKeys = Object.keys(localStorageSettings);
+    const settingsKeys = Object.keys(settings);
+    const missingKeys = settingsKeys.filter(key => !localStorageSettingsKeys.includes(key));
+    if (missingKeys.length > 0) {
+        const newSettings = { ...localStorageSettings, ...defaultSettings };
+        localStorage.setItem('settings', JSON.stringify(newSettings));
+        setSettings(newSettings);
+    }
 
     const updateSettings = async (newSettings: Partial<SettingsOptions>) => {
         try {
