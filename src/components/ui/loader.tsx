a8e2ff15870bfcc120fa04562321663f360654af
Daniel Bermann Schmidt <danielz2nd@hotmail.com>
2024-10-07 01:41:31 +0200

    update to the newest shadcn components (stolen from tauri-migration branch)

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -5,43 +5,43 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const loaderVariants = cva("animate-spin shrink-0", {
-	variants: {
-		variant: {
-			default: "stroke-foreground text-foreground",
-			white: "stroke-white text-white",
-			black: "stroke-black text-black",
-			destructive: "stroke-destructive text-destructive-foreground",
-			success: "stroke-success text-success-foreground",
-		},
-		size: {
-			md: "h-8 w-8",
-			default: "h-6 w-6",
-			sm: "h-5 w-5",
-			xs: "h-4 w-4",
-			lg: "h-10 w-10",
-			xl: "h-12 w-12",
-		},
-	},
-	defaultVariants: {
-		variant: "default",
-		size: "default",
-	},
+  variants: {
+    variant: {
+      default: "stroke-foreground text-foreground",
+      white: "stroke-white text-white",
+      black: "stroke-black text-black",
+      destructive: "stroke-destructive text-destructive-foreground",
+      success: "stroke-success text-success-foreground",
+    },
+    size: {
+      md: "h-8 w-8",
+      default: "h-6 w-6",
+      sm: "h-5 w-5",
+      xs: "h-4 w-4",
+      lg: "h-10 w-10",
+      xl: "h-12 w-12",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
 });
 
 interface LoaderProps
-	extends Omit<LucideProps, "size">,
-		VariantProps<typeof loaderVariants> {}
+  extends Omit<LucideProps, "size">,
+    VariantProps<typeof loaderVariants> {}
 
 const Loader = React.forwardRef<SVGSVGElement, LoaderProps>(
-	({ className, variant, size, ...props }, ref) => {
-		return (
-			<Loader2
-				className={cn(loaderVariants({ variant, size, className }))}
-				ref={ref}
-				{...props}
-			/>
-		);
-	},
+  ({ className, variant, size, ...props }, ref) => {
+    return (
+      <Loader2
+        className={cn(loaderVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
 );
 
 Loader.displayName = "Loader";
--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -3,36 +3,23 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-type ProgressProps = React.ComponentPropsWithoutRef<
-	typeof ProgressPrimitive.Root
-> & {
-	indicatorClassName?: string;
-	indicatorStyle?: React.CSSProperties;
-};
-
 const Progress = React.forwardRef<
-	React.ElementRef<typeof ProgressPrimitive.Root>,
-	ProgressProps
->(({ className, indicatorClassName, indicatorStyle, value, ...props }, ref) => (
-	<ProgressPrimitive.Root
-		ref={ref}
-		className={cn(
-			"relative h-4 w-full overflow-hidden rounded-full bg-secondary",
-			className,
-		)}
-		{...props}
-	>
-		<ProgressPrimitive.Indicator
-			className={cn(
-				"h-full w-full flex-1 bg-primary transition-all",
-				indicatorClassName,
-			)}
-			style={{
-				transform: `translateX(-${100 - (value || 0)}%)`,
-				...indicatorStyle,
-			}}
-		/>
-	</ProgressPrimitive.Root>
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+      className,
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="h-full w-full flex-1 bg-primary transition-all"
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
 ));
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
