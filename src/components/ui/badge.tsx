eb8c28270a0b14ce9a2fed8f85c04a36121d03d8
DanielZ1337 <danielz2nd@hotmail.com>
2024-10-07 14:53:19 +0200

    only container on all-tasks inner div and not header

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,43 +4,43 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
-  {
-    variants: {
-      variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
-        secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        destructive:
-          "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
-        purple: "text-foreground hover:bg-secondary-200",
-        success: "text-foreground hover:bg-green-600 bg-green-500",
-        outline: "text-foreground",
-      },
-      size: {
-        default: "px-2.5 py-0.5 text-xs",
-        lg: "gap-2 px-4 py-1 text-sm",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
-  },
+	"inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+	{
+		variants: {
+			variant: {
+				default:
+					"border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
+				secondary:
+					"border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+				destructive:
+					"border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
+				purple: "text-foreground hover:bg-secondary-200",
+				success: "text-foreground hover:bg-green-600 bg-green-500",
+				outline: "text-foreground",
+			},
+			size: {
+				default: "px-2.5 py-0.5 text-xs",
+				lg: "gap-2 px-4 py-1 text-sm",
+			},
+		},
+		defaultVariants: {
+			variant: "default",
+			size: "default",
+		},
+	},
 );
 
 export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
+	extends React.HTMLAttributes<HTMLDivElement>,
+		VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, size, ...props }: BadgeProps) {
-  return (
-    <div
-      className={cn(badgeVariants({ variant, size }), className)}
-      {...props}
-    />
-  );
+	return (
+		<div
+			className={cn(badgeVariants({ variant, size }), className)}
+			{...props}
+		/>
+	);
 }
 
 export { Badge, badgeVariants };
