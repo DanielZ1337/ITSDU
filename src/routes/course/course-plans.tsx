136660b0639066e5d9fdccf7c37f2d472127ec6a
DanielZ1337 <danielz2nd@hotmail.com>
2024-10-14 04:56:54 +0200

    ability to click on folder from plans and open it in app

--- src/routes/course/course-plans.tsx
+++ src/routes/course/course-plans.tsx
@@ -1,49 +1,47 @@
-import { CoursePlansSkeletonsAnimated } from "@/components/course/plans/course-plans-card-skeletons-animated";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Loader } from "@/components/ui/loader";
-import { Progress } from "@/components/ui/progress";
-import { TabButtonHoverProvider } from "@/contexts/tab-button-hover-context";
-import { useTabButtonHover } from "@/hooks/useTabButtonHover";
-import { cn } from "@/lib/utils";
-import useGETcoursePlansCount from "@/queries/courses/useGETcoursePlansCount";
-import useGETcoursePlansCurrent from "@/queries/courses/useGETcoursePlansCurrent";
-import useGETcoursePlansPast from "@/queries/courses/useGETcoursePlansPast";
-import useGETcoursePlansTopics from "@/queries/courses/useGETcoursePlansTopics";
-import useGETcoursePlansWithoutDate from "@/queries/courses/useGETcoursePlansWithoutDate";
-import { GETcoursePlansCurrent } from "@/types/api-types/courses/GETcoursePlansCurrent";
-import {
-	isSupportedResourceInApp,
-	useNavigateToResource,
-} from "@/types/api-types/extra/learning-tool-id-types";
-import { AnimatePresence, m, motion, useCycle } from "framer-motion";
-import Linkify from "linkify-react";
-import { CalendarIcon } from "lucide-react";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import { useNavigate, useParams } from "react-router-dom";
+import { CoursePlansSkeletonsAnimated } from '@/components/course/plans/course-plans-card-skeletons-animated'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import { Loader } from '@/components/ui/loader'
+import { Progress } from '@/components/ui/progress'
+import { TabButtonHoverProvider } from '@/contexts/tab-button-hover-context'
+import { useTabButtonHover } from '@/hooks/useTabButtonHover'
+import { cn } from '@/lib/utils'
+import useGETcoursePlansCount from '@/queries/courses/useGETcoursePlansCount'
+import useGETcoursePlansCurrent from '@/queries/courses/useGETcoursePlansCurrent'
+import useGETcoursePlansPast from '@/queries/courses/useGETcoursePlansPast'
+import useGETcoursePlansTopics from '@/queries/courses/useGETcoursePlansTopics'
+import useGETcoursePlansWithoutDate from '@/queries/courses/useGETcoursePlansWithoutDate'
+import { GETcoursePlansCurrent } from '@/types/api-types/courses/GETcoursePlansCurrent'
+import { isSupportedResourceInApp, useNavigateToResource } from '@/types/api-types/extra/learning-tool-id-types'
+import { ItsolutionsItslUtilsConstantsElementType } from '@/types/api-types/utils/Itsolutions.ItslUtils.Constants.ElementType'
+import { AnimatePresence, m, motion, useCycle } from 'framer-motion'
+import Linkify from 'linkify-react'
+import { CalendarIcon } from 'lucide-react'
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export default function CoursePlans() {
-	const { id } = useParams();
-	const courseId = Number(id);
+	const { id } = useParams()
+	const courseId = Number(id)
 
-	const { data, isLoading } = useGETcoursePlansCount({ courseId });
+	const { data, isLoading } = useGETcoursePlansCount({ courseId })
 
 	const tabs = [
 		{
-			id: "current",
-			label: "Current",
+			id: 'current',
+			label: 'Current',
 			value: data?.currentPlansCount,
 		},
 		{
-			id: "past",
-			label: "Past",
+			id: 'past',
+			label: 'Past',
 			value: data?.pastPlansCount,
 		},
 		{
-			id: "without-date",
-			label: "Without date",
+			id: 'without-date',
+			label: 'Without date',
 			value: data?.withoutDatePlansCount,
 		},
 		/* {
@@ -51,14 +49,14 @@ export default function CoursePlans() {
             label: "Topic",
             value: data?.topicsCount,
         } */
-	];
+	]
 
-	const [activeTab, setTabIndex] = useCycle(...tabs);
+	const [activeTab, setTabIndex] = useCycle(...tabs)
 
 	return (
-		<div className="grid items-start gap-6 p-4 text-sm font-medium">
-			<div className="flex justify-between mb-2">
-				<div className="flex">
+		<div className='grid items-start gap-6 p-4 text-sm font-medium'>
+			<div className='flex justify-between mb-2'>
+				<div className='flex'>
 					<TabButtonHoverProvider>
 						{tabs.map((tab, index) => (
 							<CoursePlansTabButton
@@ -67,9 +65,12 @@ export default function CoursePlans() {
 								active={activeTab.id === tab.id}
 								onClick={() => setTabIndex(tabs.indexOf(tab))}
 							>
-								{tab.label}{" "}
+								{tab.label}{' '}
 								{isLoading ? (
-									<Loader size={"xs"} className="ml-2" />
+									<Loader
+										size={'xs'}
+										className='ml-2'
+									/>
 								) : (
 									tab.value && `(${tab.value})`
 								)}
@@ -79,7 +80,7 @@ export default function CoursePlans() {
 				</div>
 				{/* <Button variant="outline">Table view</Button> */}
 			</div>
-			<div className="max-w-5xl mx-auto w-full">
+			<div className='max-w-5xl mx-auto w-full'>
 				<ErrorBoundary fallback={<Loader />}>
 					<Suspense fallback={<Loader />}>
 						<CoursePlansTabContent
@@ -90,86 +91,90 @@ export default function CoursePlans() {
 				</ErrorBoundary>
 			</div>
 		</div>
-	);
+	)
 }
 
-function CoursePlansTabContent({
-	courseId,
-	activeTab,
-}: { courseId: number; activeTab: string }) {
+function CoursePlansTabContent({ courseId, activeTab }: { courseId: number; activeTab: string }) {
 	switch (activeTab) {
-		case "current":
-			return <CoursePlansSwitched courseId={courseId} planType={"current"} />;
-		case "past":
-			return <CoursePlansSwitched courseId={courseId} planType={"past"} />;
-		case "without-date":
+		case 'current':
 			return (
-				<CoursePlansSwitched courseId={courseId} planType={"withoutDate"} />
-			);
-		case "topic":
-			return <CoursePlansByTopic courseId={courseId} />;
+				<CoursePlansSwitched
+					courseId={courseId}
+					planType={'current'}
+				/>
+			)
+		case 'past':
+			return (
+				<CoursePlansSwitched
+					courseId={courseId}
+					planType={'past'}
+				/>
+			)
+		case 'without-date':
+			return (
+				<CoursePlansSwitched
+					courseId={courseId}
+					planType={'withoutDate'}
+				/>
+			)
+		case 'topic':
+			return <CoursePlansByTopic courseId={courseId} />
 		default:
-			return null;
+			return null
 	}
 }
 
-function CoursePlansSwitched({
-	courseId,
-	planType,
-}: { courseId: number; planType: string }) {
+function CoursePlansSwitched({ courseId, planType }: { courseId: number; planType: string }) {
 	// Determine which API hook to use based on the planType
-	let useGETCoursePlans;
+	let useGETCoursePlans
 	switch (planType) {
-		case "current":
-			useGETCoursePlans = useGETcoursePlansCurrent;
-			break;
-		case "past":
-			useGETCoursePlans = useGETcoursePlansPast;
-			break;
-		case "withoutDate":
-			useGETCoursePlans = useGETcoursePlansWithoutDate;
-			break;
+		case 'current':
+			useGETCoursePlans = useGETcoursePlansCurrent
+			break
+		case 'past':
+			useGETCoursePlans = useGETcoursePlansPast
+			break
+		case 'withoutDate':
+			useGETCoursePlans = useGETcoursePlansWithoutDate
+			break
 		default:
-			throw new Error("Invalid plan type");
+			throw new Error('Invalid plan type')
 	}
 
 	const { data: plans, isLoading: isLoadingPlans } = useGETCoursePlans({
 		courseId,
-	});
+	})
 
-	let sortedPlans;
+	let sortedPlans
 
 	switch (planType) {
-		case "current":
+		case 'current':
 			sortedPlans = plans?.entityArray.sort((a, b) => {
-				return new Date(a.start).getTime() - new Date(b.start).getTime();
-			});
-			break;
-		case "past":
+				return new Date(a.start).getTime() - new Date(b.start).getTime()
+			})
+			break
+		case 'past':
 			sortedPlans = plans?.entityArray.sort((a, b) => {
-				return new Date(b.start).getTime() - new Date(a.start).getTime();
-			});
-			break;
-		case "withoutDate":
+				return new Date(b.start).getTime() - new Date(a.start).getTime()
+			})
+			break
+		case 'withoutDate':
 			sortedPlans = plans?.entityArray.sort((a, b) => {
-				return (
-					new Date(b.activeFromDT).getTime() -
-					new Date(a.activeFromDT).getTime()
-				);
-			});
-			break;
+				return new Date(b.activeFromDT).getTime() - new Date(a.activeFromDT).getTime()
+			})
+			break
 		default:
-			throw new Error("Invalid plan type");
+			throw new Error('Invalid plan type')
 	}
 
 	return (
-		<AnimatePresence mode="wait">
+		<AnimatePresence mode='wait'>
 			{isLoadingPlans ? (
 				<CoursePlansSkeletonsAnimated PageSize={10} />
 			) : (
 				<m.div
-					className="flex flex-col gap-4"
-					key={"plans"}
+					className='flex flex-col gap-4'
+					key={'plans'}
 					initial={{ opacity: 0, y: -20 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ delay: 0.2 }}
@@ -189,22 +194,22 @@ function CoursePlansSwitched({
 				</m.div>
 			)}
 		</AnimatePresence>
-	);
+	)
 }
 
 function CoursePlansByTopic({ courseId }: { courseId: number }) {
 	const { data: plans, isLoading: isLoadingPlans } = useGETcoursePlansTopics({
 		courseId,
-	});
+	})
 
 	return (
-		<AnimatePresence mode="wait">
+		<AnimatePresence mode='wait'>
 			{isLoadingPlans ? (
 				<CoursePlansSkeletonsAnimated PageSize={10} />
 			) : (
 				<m.div
-					className="flex flex-col gap-4"
-					key={"plans"}
+					className='flex flex-col gap-4'
+					key={'plans'}
 					initial={{ opacity: 0, y: -20 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ delay: 0.2 }}
@@ -225,68 +230,67 @@ function CoursePlansByTopic({ courseId }: { courseId: number }) {
 				</m.div>
 			)}
 		</AnimatePresence>
-	);
+	)
 }
 
-function CoursePlanCard({
-	plan,
-}: { plan: GETcoursePlansCurrent["entityArray"][number] }) {
-	const dateFormatter = new Intl.DateTimeFormat("en-GB", {
-		year: "numeric",
-		month: "short",
-		day: "numeric",
-	});
+function CoursePlanCard({ plan }: { plan: GETcoursePlansCurrent['entityArray'][number] }) {
+	const dateFormatter = new Intl.DateTimeFormat('en-GB', {
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric',
+	})
 
-	const timeFormatter = new Intl.DateTimeFormat("en-GB", {
-		hour: "numeric",
-		minute: "numeric",
-	});
+	const timeFormatter = new Intl.DateTimeFormat('en-GB', {
+		hour: 'numeric',
+		minute: 'numeric',
+	})
 
 	const isSameDay = (date1: Date, date2: Date) => {
-		return date1.toDateString() === date2.toDateString();
-	};
+		return date1.toDateString() === date2.toDateString()
+	}
 
 	const formattedTimeSpan = (start: Date, stop: Date) => {
 		if (isSameDay(start, stop)) {
-			return `${dateFormatter.format(start)} (${timeFormatter.format(start)} - ${timeFormatter.format(stop)})`;
+			return `${dateFormatter.format(start)} (${timeFormatter.format(start)} - ${timeFormatter.format(stop)})`
 		} else {
-			return `${dateFormatter.format(start)} - ${dateFormatter.format(stop)}`;
+			return `${dateFormatter.format(start)} - ${dateFormatter.format(stop)}`
 		}
-	};
+	}
 
-	const completedTopics = plan.elements.filter((topic) => topic.isCompleted);
+	const completedTopics = plan.elements.filter((topic) => topic.isCompleted)
 
-	const completedTopicsCount = completedTopics.length;
+	const completedTopicsCount = completedTopics.length
 
-	const totalTopicsCount = plan.elements.length;
+	const totalTopicsCount = plan.elements.length
 
-	const progress = (completedTopicsCount / totalTopicsCount) * 100;
+	const progress = (completedTopicsCount / totalTopicsCount) * 100
 
-	const navigate = useNavigate();
-	const navigateToResource = useNavigateToResource(navigate);
+	const navigate = useNavigate()
+	const navigateToResource = useNavigateToResource(navigate)
+
+	const { id } = useParams()
+	const courseId = Number(id)
 
 	return (
 		<Card
-			className={cn("p-2")}
+			className={cn('p-2')}
 			style={{
-				backgroundColor: plan.topic
-					? `hsl(from ${plan.topic.color} h s l / 60%)`
-					: "hsl(var(--foreground)/10%)",
+				backgroundColor: plan.topic ? `hsl(from ${plan.topic.color} h s l / 60%)` : 'hsl(var(--foreground)/10%)',
 				borderColor: plan.topic && plan.topic.borderColor,
 			}}
 		>
-			<CardContent className="p-4">
-				<div className="flex justify-between">
+			<CardContent className='p-4'>
+				<div className='flex justify-between'>
 					<div>
-						<h3 className="text-lg font-semibold mb-2 flex items-center">
+						<h3 className='text-lg font-semibold mb-2 flex items-center'>
 							{plan.title}
 							{plan.topic && (
 								<Badge
-									variant="secondary"
-									className="ml-2 rounded-small"
+									variant='secondary'
+									className='ml-2 rounded-small'
 									style={{
 										backgroundColor: plan.topic.color,
-										color: "black",
+										color: 'black',
 									}}
 								>
 									{plan.topic.name}
@@ -294,30 +298,28 @@ function CoursePlanCard({
 							)}
 						</h3>
 						{plan.description && (
-							<p className="text-sm mb-2">
+							<p className='text-sm mb-2'>
 								{/<\/?[a-z][\s\S]*>/i.test(plan.description) ? (
 									<div dangerouslySetInnerHTML={{ __html: plan.description }} />
 								) : (
 									<Linkify
 										options={{
 											render: ({ attributes, content }) => {
-												const { href, ...props } = attributes;
+												const { href, ...props } = attributes
 												return (
 													<a
-														target={"_blank"}
-														rel={"noopener noreferrer"}
-														className={
-															"text-foreground hover:underline cursor-pointer"
-														}
+														target={'_blank'}
+														rel={'noopener noreferrer'}
+														className={'text-foreground hover:underline cursor-pointer'}
 														onClick={(e) => {
-															e.stopPropagation();
-															window.app.openExternal(href, false);
+															e.stopPropagation()
+															window.app.openExternal(href, false)
 														}}
 														{...props}
 													>
 														{content}
 													</a>
-												);
+												)
 											},
 										}}
 									>
@@ -329,59 +331,58 @@ function CoursePlanCard({
 					</div>
 					{/* <Button>Continue</Button> */}
 				</div>
-				<div className="flex items-center">
+				<div className='flex items-center'>
 					<CalendarIcon
-						className="w-6 h-6"
+						className='w-6 h-6'
 						style={{
-							color: plan.topic ? plan.topic.color : "var(--foreground)",
+							color: plan.topic ? plan.topic.color : 'var(--foreground)',
 						}}
 					/>
-					<span className="ml-2 text-sm">
-						{formattedTimeSpan(new Date(plan.start), new Date(plan.stop))}
-					</span>
+					<span className='ml-2 text-sm'>{formattedTimeSpan(new Date(plan.start), new Date(plan.stop))}</span>
 					<Progress
-						className="w-1/2 ml-4"
+						className='w-1/2 ml-4'
 						value={progress}
 						indicatorStyle={{
 							backgroundColor:
 								completedTopicsCount === totalTopicsCount
-									? "hsl(var(--nextui-success)/80%)"
+									? 'hsl(var(--nextui-success)/80%)'
 									: plan.topic && plan.topic.color,
 						}}
 						style={{
-							backgroundColor: plan.topic
-								? `hsl(from ${plan.topic.color} h s l / 30%)`
-								: "hsl(var(--foreground)/10%)",
+							backgroundColor: plan.topic ? `hsl(from ${plan.topic.color} h s l / 30%)` : 'hsl(var(--foreground)/10%)',
 						}}
 					/>
 					{completedTopicsCount > 0 && (
-						<span className="ml-2 text-sm">
+						<span className='ml-2 text-sm'>
 							{completedTopicsCount}/{totalTopicsCount} completed
 						</span>
 					)}
 				</div>
-				<div className="mt-4">
+				<div className='mt-4'>
 					<ul
-						className="grid grid-cols-1 gap-2"
+						className='grid grid-cols-1 gap-2'
 						style={{
-							gridTemplateColumns: "repeat(auto-fill, minmax(300px, 30fr))",
+							gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 30fr))',
 						}}
 					>
 						{plan.elements.map((element, index) => (
-							<li key={element.id} className="hover:cursor-pointer pr-2">
-								<span className="flex items-center gap-2">
+							<li
+								key={element.id}
+								className='hover:cursor-pointer pr-2'
+							>
+								<span className='flex items-center gap-2'>
 									<img
-										loading="lazy"
-										className="inline-block w-6 h-6 mr-2 shrink-0"
+										loading='lazy'
+										className='inline-block w-6 h-6 mr-2 shrink-0'
 										src={element.iconUrl}
 										alt={element.title}
 									/>
 									<a
-										className="text-foreground hover:underline truncate"
-										target="_blank"
-										rel="noopener noreferrer"
+										className='text-foreground hover:underline truncate'
+										target='_blank'
+										rel='noopener noreferrer'
 										onClick={async (e) => {
-											e.preventDefault();
+											e.preventDefault()
 
 											const resource = {
 												ElementId: element.id,
@@ -389,15 +390,14 @@ function CoursePlanCard({
 												Title: element.title,
 												Url: element.url,
 												LearningToolId: element.learningToolId,
-											};
+												ElementType: element.isFolder ? ItsolutionsItslUtilsConstantsElementType.Folder : undefined,
+												CourseId: courseId,
+											}
 
 											if (isSupportedResourceInApp(resource)) {
-												navigateToResource(resource);
+												navigateToResource(resource)
 											} else {
-												await window.app.openExternal(
-													`https://sdu.itslearning.com${element.url}`,
-													true,
-												);
+												await window.app.openExternal(`https://sdu.itslearning.com${element.url}`, true)
 											}
 										}}
 									>
@@ -410,7 +410,7 @@ function CoursePlanCard({
 				</div>
 			</CardContent>
 		</Card>
-	);
+	)
 }
 
 export function CoursePlansTabButton({
@@ -419,22 +419,22 @@ export function CoursePlansTabButton({
 	onClick,
 	children,
 }: {
-	id: string;
-	active: boolean;
-	onClick: () => void;
-	children: React.ReactNode;
+	id: string
+	active: boolean
+	onClick: () => void
+	children: React.ReactNode
 }) {
-	const [hoveredTab, setHoveredTab] = useTabButtonHover();
+	const [hoveredTab, setHoveredTab] = useTabButtonHover()
 
 	return (
-		<div className="relative flex items-center justify-center">
+		<div className='relative flex items-center justify-center'>
 			<Button
-				variant={"none"}
+				variant={'none'}
 				onClick={onClick}
-				size={"sm"}
+				size={'sm'}
 				className={cn(
-					"capitalize h-11 relative hover:text-white transition-all duration-200 ease-in-out",
-					active ? "text-white" : "text-gray-600",
+					'capitalize h-11 relative hover:text-white transition-all duration-200 ease-in-out',
+					active ? 'text-white' : 'text-gray-600'
 				)}
 				onMouseEnter={() => setHoveredTab(id)}
 				onMouseLeave={() => setHoveredTab(null)}
@@ -442,19 +442,19 @@ export function CoursePlansTabButton({
 				{children}
 				{hoveredTab === id && (
 					<motion.div
-						layoutId="active-plans-tab-indicator"
+						layoutId='active-plans-tab-indicator'
 						transition={{ duration: 0.2 }}
-						className="absolute bottom-0 left-0 right-0 h-1 bg-purple-500 rounded-full"
+						className='absolute bottom-0 left-0 right-0 h-1 bg-purple-500 rounded-full'
 					/>
 				)}
 				{active && !hoveredTab && (
 					<motion.div
-						layoutId="active-plans-tab-indicator"
+						layoutId='active-plans-tab-indicator'
 						transition={{ duration: 0.2 }}
-						className="absolute bottom-0 left-0 right-0 h-1 bg-purple-500 rounded-full"
+						className='absolute bottom-0 left-0 right-0 h-1 bg-purple-500 rounded-full'
 					/>
 				)}
 			</Button>
 		</div>
-	);
+	)
 }
