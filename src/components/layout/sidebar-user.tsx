8126b431190d33f1dec1b52d00e238f6cae14784
DanielZ1337 <danielz2nd@hotmail.com>
2024-10-24 00:58:29 +0200

    feature: sidebar-user dropdown open on hover, no tooltip. fix settings dialog after updating to newest shadcn components

--- src/components/layout/sidebar-user.tsx
+++ src/components/layout/sidebar-user.tsx
@@ -1,51 +1,36 @@
-import SettingsDropdown from "@/components/settings/settings-dropdown";
-import {
-	Tooltip,
-	TooltipContent,
-	TooltipProvider,
-	TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { useUser } from "@/hooks/atoms/useUser";
-import { MoreVertical } from "lucide-react";
-import { useState } from "react";
-import ProfileAvatar from "../profile-avatar";
+import SettingsDropdown from '@/components/settings/settings-dropdown'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+import { useUser } from '@/hooks/atoms/useUser'
+import { MoreVertical } from 'lucide-react'
+import { useState } from 'react'
+import ProfileAvatar from '../profile-avatar'
+import { cn } from '@nextui-org/react'
+import { buttonVariants } from '@/components/ui/button'
 
 export default function SidebarUser() {
-	const user = useUser();
+	const user = useUser()
 
-	const [isOpen, setIsOpen] = useState(false);
-
-	const [isOpenSettings, setIsOpenSettings] = useState(false);
+	const [isOpen, setIsOpen] = useState(false)
+	const [isHovering, setIsHovering] = useState(false)
 
 	return (
-		<Tooltip open={isOpen || isOpenSettings} onOpenChange={setIsOpen}>
-			<TooltipTrigger>
-				<ProfileAvatar
-					src={user?.ProfileImageUrl}
-					name={user?.FullName}
-					className={"w-10 h-10 border-2 border-primary/30"}
-					classNameFallback={"bg-foreground/10 font-normal text-sm"}
-				/>
-				{/* <div className={`pl-4 flex justify-between items-center overflow-hidden transition-all`}>
-					<div className='pr-4 leading-4'>
-						<h4 className='whitespace-pre-wrap break-all font-semibold line-clamp-1'>{user?.FullName}</h4>
-					</div>
-					<SettingsDropdown />
-				</div> */}
-			</TooltipTrigger>
-			<TooltipContent side="right">
-				<div className="p-2 flex justify-between items-center overflow-hidden transition-all">
-					<div className="pr-4 leading-4">
-						<h4 className="whitespace-pre-wrap break-all font-semibold line-clamp-1">
-							{user?.FullName}
-						</h4>
-					</div>
-					<SettingsDropdown
-						triggerComponent={<MoreVertical className="h-6 w-6 p-0.5" />}
-						onOpenChange={setIsOpenSettings}
+		<SettingsDropdown
+			triggerComponent={
+				<button
+					onMouseEnter={() => setIsHovering(true)}
+					onMouseLeave={() => setIsHovering(false)}
+					className='w-full h-full py-2 px-2.5'
+				>
+					<ProfileAvatar
+						src={user?.ProfileImageUrl}
+						name={user?.FullName}
+						className={'w-10 h-10 border-2 border-primary/30'}
+						classNameFallback={'bg-foreground/10 font-normal text-sm'}
 					/>
-				</div>
-			</TooltipContent>
-		</Tooltip>
-	);
+				</button>
+			}
+			isOpen={isHovering || isOpen}
+			onOpenChange={setIsOpen}
+		/>
+	)
 }
--- src/components/settings/settings-dropdown.tsx
+++ src/components/settings/settings-dropdown.tsx
@@ -1,5 +1,5 @@
-import SettingsDropdownUserFullname from "@/components/settings/settings-dropdown-user-fullname.tsx";
-import { buttonVariants } from "@/components/ui/button.tsx";
+import SettingsDropdownUserFullname from '@/components/settings/settings-dropdown-user-fullname.tsx'
+import { buttonVariants } from '@/components/ui/button.tsx'
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -8,91 +8,95 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuShortcut,
 	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu.tsx";
-import { useAboutModal } from "@/hooks/atoms/useAboutModal.ts";
-import { useShowSettingsModal } from "@/hooks/atoms/useSettingsModal.ts";
-import { useVersion } from "@/hooks/atoms/useVersion.ts";
-import { cn, isMacOS } from "@/lib/utils.ts";
-import { useAtom } from "jotai";
-import { MoreVertical } from "lucide-react";
-import { useTheme } from "next-themes";
-import { Suspense, useCallback, useEffect, useState } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import { useNavigate } from "react-router-dom";
-import { browseNavigationAtom as showBrowseNavAtom } from "../../atoms/browse-navigation.ts";
+} from '@/components/ui/dropdown-menu.tsx'
+import { useAboutModal } from '@/hooks/atoms/useAboutModal.ts'
+import { useShowSettingsModal } from '@/hooks/atoms/useSettingsModal.ts'
+import { useVersion } from '@/hooks/atoms/useVersion.ts'
+import { cn, isMacOS } from '@/lib/utils.ts'
+import { useAtom } from 'jotai'
+import { MoreVertical } from 'lucide-react'
+import { useTheme } from 'next-themes'
+import { Suspense, useCallback, useEffect, useState } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import { useNavigate } from 'react-router-dom'
+import { browseNavigationAtom as showBrowseNavAtom } from '../../atoms/browse-navigation.ts'
 
 export default function SettingsDropdown({
 	triggerComponent,
 	onOpenChange,
+	isOpen,
 }: {
-	triggerComponent: React.ReactNode;
-	onOpenChange?: (isOpen: boolean) => void;
+	triggerComponent: React.ReactNode
+	onOpenChange?: (isOpen: boolean) => void
+	isOpen?: boolean
 }) {
-	const navigate = useNavigate();
-	const { theme, setTheme } = useTheme();
+	const navigate = useNavigate()
+	const { theme, setTheme } = useTheme()
 
 	const handleDarkModeToggle = useCallback(async () => {
-		const isDarkMode = await window.darkMode.toggle();
-		setTheme(isDarkMode ? "dark" : "light");
-	}, [setTheme]);
+		const isDarkMode = await window.darkMode.toggle()
+		setTheme(isDarkMode ? 'dark' : 'light')
+	}, [setTheme])
 
-	const { toggleSettingsModal } = useShowSettingsModal();
-	const { version } = useVersion();
-	const [showBrowseNav, setShowBrowseNav] = useAtom(showBrowseNavAtom);
-	const { toggleAboutModal } = useAboutModal();
+	const { toggleSettingsModal } = useShowSettingsModal()
+	const { version } = useVersion()
+	const [showBrowseNav, setShowBrowseNav] = useAtom(showBrowseNavAtom)
+	const { toggleAboutModal } = useAboutModal()
 	const commandOrControl = () => {
 		if (isMacOS()) {
-			return "⌘";
+			return '⌘'
 		} else {
-			return "Ctrl ";
+			return 'Ctrl '
 		}
-	};
+	}
 
-	const [isOpen, setIsOpen] = useState(false);
+	const [_isOpen, _setIsOpen] = useState(false)
 
 	useEffect(() => {
-		onOpenChange?.(isOpen);
-	}, [isOpen, onOpenChange]);
+		if (isOpen !== undefined) {
+			_setIsOpen(isOpen)
+		}
+	}, [isOpen, _setIsOpen])
+
+	useEffect(() => {
+		onOpenChange?.(_isOpen)
+	}, [_isOpen, onOpenChange])
 
 	function dropdownItemOnClick(
 		e: React.MouseEvent<HTMLButtonElement | HTMLDivElement, MouseEvent>,
-		callback: () => void,
+		callback: () => void
 	) {
-		e.stopPropagation();
-		e.preventDefault();
-		callback();
+		e.stopPropagation()
+		e.preventDefault()
+		callback()
 	}
 
 	return (
-		<DropdownMenu modal={false} open={isOpen} onOpenChange={setIsOpen}>
+		<DropdownMenu
+			modal={false}
+			open={_isOpen}
+			onOpenChange={_setIsOpen}
+		>
 			<DropdownMenuTrigger
 				className={cn(
 					buttonVariants({
-						variant: "ghost",
-						size: "icon",
-					}),
+						variant: 'ghost',
+						size: 'icon',
+					})
 				)}
 				asChild
-				onClick={(e) => {
-					e.stopPropagation();
-					e.preventDefault();
-				}}
 			>
 				{triggerComponent}
 			</DropdownMenuTrigger>
-			<DropdownMenuContent>
-				<DropdownMenuLabel className={"flex justify-between items-center"}>
-					<span className={"text-sm"}>Settings</span>
-					<span className={"text-xs text-gray-500"}>{version}</span>
+			<DropdownMenuContent side='right'>
+				<DropdownMenuLabel className={'flex justify-between items-center'}>
+					<span className={'text-sm'}>Settings</span>
+					<span className={'text-xs text-gray-500'}>{version}</span>
 				</DropdownMenuLabel>
 				{/*user name*/}
-				<DropdownMenuLabel className={"font-normal"}>
-					<ErrorBoundary
-						fallback={<div className={"animate-pulse"}>Loading...</div>}
-					>
-						<Suspense
-							fallback={<div className={"animate-pulse"}>Loading...</div>}
-						>
+				<DropdownMenuLabel className={'font-normal'}>
+					<ErrorBoundary fallback={<div className={'animate-pulse'}>Loading...</div>}>
+						<Suspense fallback={<div className={'animate-pulse'}>Loading...</div>}>
 							<SettingsDropdownUserFullname />
 						</Suspense>
 					</ErrorBoundary>
@@ -101,19 +105,17 @@ export default function SettingsDropdown({
 				<DropdownMenuItem
 					onClick={(e) =>
 						dropdownItemOnClick(e, () => {
-							handleDarkModeToggle();
+							handleDarkModeToggle()
 						})
 					}
 				>
-					<span className={"text-sm"}>
-						{theme === "dark" ? "Light" : "Dark"}
-					</span>
+					<span className={'text-sm'}>{theme === 'dark' ? 'Light' : 'Dark'}</span>
 					<DropdownMenuShortcut>{commandOrControl()}T</DropdownMenuShortcut>
 				</DropdownMenuItem>
 				<DropdownMenuItem
 					onClick={(e) =>
 						dropdownItemOnClick(e, () => {
-							toggleSettingsModal();
+							toggleSettingsModal()
 						})
 					}
 				>
@@ -123,7 +125,7 @@ export default function SettingsDropdown({
 				<DropdownMenuItem
 					onClick={(e) =>
 						dropdownItemOnClick(e, () => {
-							setShowBrowseNav(!showBrowseNav);
+							setShowBrowseNav(!showBrowseNav)
 						})
 					}
 				>
@@ -135,7 +137,7 @@ export default function SettingsDropdown({
 				<DropdownMenuItem
 					onClick={(e) =>
 						dropdownItemOnClick(e, () => {
-							toggleAboutModal();
+							toggleAboutModal()
 						})
 					}
 				>
@@ -145,29 +147,25 @@ export default function SettingsDropdown({
 				<DropdownMenuItem
 					onClick={() => {
 						window.auth.logout().then(() => {
-							console.log("logged out");
-						});
+							console.log('logged out')
+						})
 					}}
-					className={
-						"hover:!bg-destructive focus:!bg-destructive hover:!text-white"
-					}
+					className={'hover:!bg-destructive focus:!bg-destructive hover:!text-white'}
 				>
 					<span>Sign out</span>
 				</DropdownMenuItem>
 				<DropdownMenuItem
 					onClick={() => {
 						window.app.exit().then((r) => {
-							console.log(r);
-						});
+							console.log(r)
+						})
 					}}
-					className={
-						"hover:!bg-destructive focus:!bg-destructive hover:!text-white"
-					}
+					className={'hover:!bg-destructive focus:!bg-destructive hover:!text-white'}
 				>
 					<span>Exit</span>
 					<DropdownMenuShortcut>{commandOrControl()}Q</DropdownMenuShortcut>
 				</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
-	);
+	)
 }
